d6308c6885d72107d83584df753109ff1cb30158
sivakumargsk <sivakumar.gsk@technoidentity.com>
2016-08-17 15:20:27 +0530

    routes are created with silk

--- src/cljs/cljs_boot_starter/routes.cljs
+++ src/cljs/cljs_boot_starter/routes.cljs
@@ -1,29 +1,57 @@
 (ns cljs-boot-starter.routes
-  (:require-macros [secretary.core :refer [defroute]])
+  ;; (:require-macros [secretary.core :refer [defroute]])
   (:import goog.History)
-  (:require [secretary.core :as secretary]
-            [goog.events :as events]
-            [goog.history.EventType :as EventType]
-            [re-frame.core :as re-frame]))
+  (:require
+   ;; [secretary.core :as secretary]
+   [clojure.set :refer [rename-keys]]
+   [domkm.silk :as silk]
+   [pushy.core :as pushy]
+   [goog.events :as events]
+   [goog.history.EventType :as EventType]
+   [re-frame.core :as re-frame]))
 
-(defn hook-browser-navigation! []
-  (doto (History.)
-    (events/listen
-     EventType/NAVIGATE
-     (fn [event]
-       (secretary/dispatch! (.-token event))))
-    (.setEnabled true)))
+;; (defn hook-browser-navigation! []
+;;   (doto (History.)
+;;     (events/listen
+;;      EventType/NAVIGATE
+;;      (fn [event]
+;;        (secretary/dispatch! (.-token event))))
+;;     (.setEnabled true)))
+
+;; (defn app-routes []
+;;   (secretary/set-config! :prefix "#")
+;;   ;; --------------------
+;;   ;; define routes here
+;;   (defroute "/" []
+;;     (re-frame/dispatch [:set-active-panel :home-panel]))
+
+;;   (defroute "/about" []
+;;     (re-frame/dispatch [:set-active-panel :about-panel]))
+
+
+;;   ;; --------------------
+;;   (hook-browser-navigation!))
+
+
+
+(def routes (silk/routes [[:home [[]]]
+                          [:about [["about"]]]]))
+
+(defn- parse-url [url]
+  (silk/arrive routes url))
+
+(defn- sanitize-silk-keywords [matched-route]
+  (rename-keys matched-route {:domkm.silk/name    :name
+                              :domkm.silk/pattern :pattern
+                              :domkm.silk/routes  :routes
+                              :domkm.silk/url     :url}))
+
+(defn- dispatch-route [matched-route]
+  (let [matched-route (sanitize-silk-keywords matched-route)
+        panel-name (keyword (str (name (:name matched-route)) "-panel"))]
+    (re-frame/dispatch [:set-active-panel panel-name])))
 
 (defn app-routes []
-  (secretary/set-config! :prefix "#")
-  ;; --------------------
-  ;; define routes here
-  (defroute "/" []
-    (re-frame/dispatch [:set-active-panel :home-panel]))
+  (pushy/start! (pushy/pushy dispatch-route parse-url)))
 
-  (defroute "/about" []
-    (re-frame/dispatch [:set-active-panel :about-panel]))
-
-
-  ;; --------------------
-  (hook-browser-navigation!))
+(def url-for (partial silk/depart routes))
