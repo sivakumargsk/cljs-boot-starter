3d3190736a473668f0180d3ed8296c1ee7fc4900
sivakumargsk <sivakumar.gsk@technoidentity.com>
2016-08-17 14:44:23 +0530

    Removing hard-code urls with help of bidi routes
    inplace of hard-coded url-stings use routes/url-for function

--- src/cljs/cljs_boot_starter/views.cljs
+++ src/cljs/cljs_boot_starter/views.cljs
@@ -1,36 +1,61 @@
 (ns cljs-boot-starter.views
-  (:require [re-frame.core :as re-frame]))
+  (:require [re-frame.core :as re-frame]
+            [cljs-boot-starter.routes :as routes]))
 
 
-;; home
+;; ;; home
 
+;; (defn home-panel []
+;;   (let [name (re-frame/subscribe [:name])]
+;;     (fn []
+;;       [:div (str "Hello from " @name ". This is the Home Page.")
+;;        [:div [:a {:href "#/about"} "go to About Page"]]])))
+
+
+;; ;; about
+
+;; (defn about-panel []
+;;   (fn []
+;;     [:div "This is the About Page."
+;;      [:div [:a {:href "#/"} "go to Home Page"]]]))
+
+
+;; ;; main
+
+;; (defmulti panels identity)
+;; (defmethod panels :home-panel [] [home-panel])
+;; (defmethod panels :about-panel [] [about-panel])
+;; (defmethod panels :default [] [:div])
+
+;; (defn show-panel
+;;   [panel-name]
+;;   [panels panel-name])
+
+;; (defn main-panel []
+;;   (let [active-panel (re-frame/subscribe [:active-panel])]
+;;     (fn []
+;;       [show-panel @active-panel])))
+
+
+;; --------------------
 (defn home-panel []
   (let [name (re-frame/subscribe [:name])]
     (fn []
       [:div (str "Hello from " @name ". This is the Home Page.")
-       [:div [:a {:href "#/about"} "go to About Page"]]])))
-
-
-;; about
+       [:div [:a {:href (routes/url-for :about)} "go to About Page"]]])))
 
 (defn about-panel []
   (fn []
     [:div "This is the About Page."
-     [:div [:a {:href "#/"} "go to Home Page"]]]))
+     [:div [:a {:href (routes/url-for :home)} "go to Home Page"]]]))
 
-
-;; main
-
+;; --------------------
 (defmulti panels identity)
 (defmethod panels :home-panel [] [home-panel])
 (defmethod panels :about-panel [] [about-panel])
 (defmethod panels :default [] [:div])
 
-(defn show-panel
-  [panel-name]
-  [panels panel-name])
-
 (defn main-panel []
   (let [active-panel (re-frame/subscribe [:active-panel])]
     (fn []
-      [show-panel @active-panel])))
+      (panels @active-panel))))
